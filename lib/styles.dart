cae4ae4af97c40f95f1f70db52f9d2a1a69326ed
tarturr <tartur.dev@gmail.com>
2024-02-20 09:23:04 +0100

    Added font Comfortaa-Light.ttf.

--- lib/styles.dart
+++ lib/styles.dart
@@ -3,6 +3,7 @@ import 'package:jaspr/jaspr.dart';
 /// The main styles for this app.
 const styles = [
   StyleRule.import('https://fonts.googleapis.com/css?family=Roboto'),
+  StyleRule.fontFace(fontFamily: 'Comfortaa', url: 'assets/font/Comfortaa-Light.ttf'),
   StyleRule(
     // Applies these styles to the html and body elements.
     selector: Selector.list([Selector.tag('html'), Selector.tag('body')]),
