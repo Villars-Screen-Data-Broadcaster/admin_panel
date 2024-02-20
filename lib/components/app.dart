7852017649be81759d0224778c1f9e000ec21a26
tarturr <tartur.dev@gmail.com>
2024-02-20 21:56:58 +0100

    Created first Home route.

--- lib/components/app.dart
+++ lib/components/app.dart
@@ -1,15 +1,13 @@
+import 'package:admin_panel/components/home/home.dart';
 import 'package:jaspr/jaspr.dart';
+import 'package:jaspr_router/jaspr_router.dart';
 
-/// The root interactive component for this app.
-///
-/// This is automatically rendered on the client by using the @client annotation.
 @client
 class App extends StatelessComponent {
   @override
   Iterable<Component> build(BuildContext context) sync* {
-    // Renders a <p> element with 'Hello World' content.
-    yield p([
-      text('Hello World'),
+    yield Router(routes: [
+      Route(path: '/', builder: (context, state) => HomePage()),
     ]);
   }
 }
