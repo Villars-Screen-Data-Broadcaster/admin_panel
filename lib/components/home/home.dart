5fbeb01e9a5485ed5c7d774aa11b428c3cb5f359
tarturr <tartur.dev@gmail.com>
2024-02-20 21:57:23 +0100

    Started creation of the HomePage class.

---
+++ lib/components/home/home.dart
@@ -0,0 +1,9 @@
+import 'package:jaspr/jaspr.dart';
+import 'package:admin_panel/components/home/navigation_bar.dart';
+
+class HomePage extends StatelessComponent {
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield SidebarNavigation();
+  }
+}
