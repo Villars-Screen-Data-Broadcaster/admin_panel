4b435cfcd2be9e5aa5ef137da7ad5713bca1c008
tarturr <tartur.dev@gmail.com>
2024-02-26 17:35:44 +0100

    Added the use of the Section class.

--- lib/components/home/navigation_bar/navigation_bar.dart
+++ lib/components/home/navigation_bar/navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:admin_panel/components/home/navigation_bar/sections.dart';
 import 'package:jaspr/jaspr.dart';
 
 import 'common.dart';
@@ -13,7 +14,26 @@ class _SidebarNavigationState extends ZippableComponentState {
   Unit _width;
 
   _SidebarNavigationState()
-      : _components = [Header()],
+      : _components = [
+            Header(),
+            Section.selected(
+                icon: 'home',
+                label: 'Accueil',
+                href: ''
+            ),
+            Section(
+                icon: 'file',
+                label: 'Fichiers',
+                href: 'files',
+                index: 2
+            ),
+            Section(
+                icon: 'desktop',
+                label: 'Ecrans',
+                href: 'screens',
+                index: 3
+            ),
+        ],
         _width = Unit.pixels(100);
 
   @override
@@ -29,6 +49,7 @@ class _SidebarNavigationState extends ZippableComponentState {
                 position: Position.fixed(top: Unit.zero, left: Unit.zero),
                 transition: Transition('', duration: transition)
             ),
+            Styles.flexbox(direction: FlexDirection.column),
             Styles.background(color: darkMarine)
           ]),
           events: {
