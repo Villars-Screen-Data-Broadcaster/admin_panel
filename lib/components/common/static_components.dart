18048f0ca1182332a3a77be1dfdff9c2276ec346
tarturr <tartur.dev@gmail.com>
2024-03-02 14:04:34 +0100

    Moved HoverableButton class to static_components.dart and added new Table class.

--- lib/components/common/static_components.dart
+++ lib/components/common/static_components.dart
@@ -1,5 +1,7 @@
-import 'package:admin_panel/components/common/navigation_bar/common.dart';
 import 'package:jaspr/jaspr.dart';
+
+import 'native_components.dart';
+import 'navigation_bar/common.dart';
 
 
 class PageHeader extends StatelessComponent {
@@ -68,6 +70,121 @@ class _PageHeaderDescription extends StatelessComponent {
                 )
             )
           ])
+      )
+    ];
+  }
+}
+
+class Table extends StatelessComponent {
+  final Component _thead;
+  final Component _tbody;
+  final Styles? _styles;
+
+  Table({required Map<String, TableRowWithButton> columns, Styles? styles})
+      : _thead = thead([
+          tr([
+            for (var header in columns.keys)
+              th(
+                  [text(header)],
+                  scope: 'col',
+                  styles: Styles.combine([
+                    Styles.text(fontSize: Unit.em(1.5)),
+                    Styles.background(color: Colors.lightGrey)
+                  ])
+              )
+          ])
+        ]),
+        _tbody = tbody([tr(columns.values.toList())]),
+        _styles = styles;
+
+  @override
+  Iterable<Component> build(BuildContext context) {
+    return [
+      table([_thead, _tbody], styles: _styles)
+    ];
+  }
+}
+
+class TableRowWithButton extends StatelessComponent {
+  final Component _description;
+  final HoverableButton _button;
+
+  TableRowWithButton({required String description, required HoverableButton button})
+      : _description = p([text(description)], styles: Styles.box(margin: EdgeInsets.only(bottom: Unit.pixels(50)))),
+        _button = button;
+
+  @override
+  Iterable<Component> build(BuildContext context) {
+    return [
+      td(
+          [_description, _button],
+          styles: Styles.combine([
+            Styles.text(align: TextAlign.center),
+            Styles.box(border: Border.all(BorderSide(
+                style: BorderStyle.solid,
+                color: Colors.lightSlateGray,
+                width: Unit.pixels(2)
+            )))
+          ])
+      )
+    ];
+  }
+
+}
+
+class HoverableButton extends StatefulComponent {
+  final String label;
+  final String href;
+  final Color bgColor;
+  final Color hoverBgColor;
+
+  HoverableButton(this.label, {required this.href, required this.bgColor, required this.hoverBgColor});
+
+  @override
+  State<StatefulComponent> createState() => _HoverableButtonState(label, href, bgColor, hoverBgColor);
+}
+
+class _HoverableButtonState extends State<HoverableButton> {
+  final String label;
+  final String href;
+  final Color bgColor;
+  final Color hoverBgColor;
+
+  Color currentBgColor;
+
+  _HoverableButtonState(this.label, this.href, this.bgColor, this.hoverBgColor)
+      : currentBgColor = bgColor;
+
+  void hover() => setState(() {
+    currentBgColor = hoverBgColor;
+  });
+
+  void unhover() => setState(() {
+    currentBgColor = bgColor;
+  });
+
+  @override
+  Iterable<Component> build(BuildContext context) {
+    return [
+      a(
+          [text(label)],
+          href: href,
+          styles: Styles.combine([
+            Styles.text(
+                color: Colors.white,
+                decoration: TextDecoration(line: TextDecorationLine.none)
+            ),
+            Styles.box(
+                padding: EdgeInsets.all(Unit.pixels(15)),
+                radius: BorderRadius.all(Radius.circular(Unit.pixels(4))),
+                transition: Transition('', duration: 150)
+            ),
+            Styles.background(color: currentBgColor)
+          ]),
+          events: {
+            'mouseover': (event) => hover(),
+            'mouseleave': (event) => unhover(),
+          }
       )
     ];
   }
