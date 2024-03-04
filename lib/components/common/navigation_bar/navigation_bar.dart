fc6a894f477c2527606182fdd1cab4a3c73d2713
tarturr <tartur.dev@gmail.com>
2024-03-04 12:09:49 +0100

    Using new CSS style to build a new display of the navbar, editing some common components and removing the unnecessary ones.

--- lib/components/common/navigation_bar/navigation_bar.dart
+++ lib/components/common/navigation_bar/navigation_bar.dart
@@ -1,100 +1,56 @@
-import 'package:admin_panel/components/common/navigation_bar/sections.dart';
+import 'package:admin_panel/components/common/navigation_bar/common.dart';
 import 'package:jaspr/jaspr.dart';
 
-import 'common.dart';
-import 'header.dart';
 
+class SideNavigationMenu extends StatelessComponent {
+  final String index;
 
-class SideNavigationMenu extends ZippableComponent {
-  SideNavigationMenu(String index) : super(_SideNavigationMenuState(index));
-}
-
-class _SideNavigationMenuState extends ZippableComponentState {
-  final List<ZippableComponent> _components;
-  Unit _width;
-
-  _SideNavigationMenuState(String index)
-      : _components = [
-            Header(),
-            index == 'home'
-                ? Section.selected(
-                    icon: 'home',
-                    label: 'Accueil',
-                    href: '/'
-                )
-                : Section(
-                    icon: 'home',
-                    label: 'Accueil',
-                    href: '/'
-                ),
-            index == 'files'
-                ? Section.selected(
-                    icon: 'file',
-                    label: 'Fichiers',
-                    href: '/files',
-                    index: 2
-                )
-                : Section(
-                    icon: 'file',
-                    label: 'Fichiers',
-                    href: '/files',
-                    index: 2
-                ),
-            index == 'screens'
-                ? Section.selected(
-                    icon: 'desktop',
-                    label: 'Ecrans',
-                    href: '/screens',
-                    index: 3
-                )
-                : Section(
-                    icon: 'desktop',
-                    label: 'Ecrans',
-                    href: '/screens',
-                    index: 3
-                )
-        ],
-        _width = Unit.pixels(100);
+  SideNavigationMenu({required this.index});
 
   @override
   Iterable<Component> build(BuildContext context) {
     return [
       section(
-          _components,
-          styles: Styles.combine([
-            Styles.box(
-                width: _width,
-                height: Unit.vh(100),
-                display: Display.flex,
-                position: Position.fixed(top: Unit.zero, left: Unit.zero),
-                transition: Transition('', duration: transition)
+          [
+            LabeledComponent.extraClasses(
+                classes: 'header',
+                component: img(
+                    src: 'assets/img/lycee.jpg',
+                    classes: 'component'
+                ),
+                label: 'VSDB - Admin Panel',
+                href: '/'
             ),
-            Styles.flexbox(direction: FlexDirection.column),
-            Styles.background(color: darkMarine)
-          ]),
-          events: {
-            'mouseover': (event) => unzip(),
-            'mouseleave': (event) => zip(),
-          }
+            LabeledComponent(
+                component: FontAwesomeIcon.extraClasses(
+                    icon: 'home',
+                    classes: 'component'
+                ),
+                label: 'Accueil',
+                href: '/',
+                selected: index == 'home'
+            ),
+            LabeledComponent(
+                component: FontAwesomeIcon.extraClasses(
+                    icon: 'file',
+                    classes: 'component'
+                ),
+                label: 'Fichiers',
+                href: '/files',
+                selected: index == 'files'
+            ),
+            LabeledComponent(
+                component: FontAwesomeIcon.extraClasses(
+                    icon: 'desktop',
+                    classes: 'component'
+                ),
+                label: 'Ecrans',
+                href: '/screens',
+                selected: index == 'screens'
+            )
+          ],
+          id: 'nav-bar'
       )
     ];
   }
-
-  @override
-  void unzip() => setState(() {
-    _width = Unit.vw(25);
-
-    for (var component in _components) {
-      component.unzip();
-    }
-  });
-
-  @override
-  void zip() => setState(() {
-    _width = Unit.pixels(100);
-
-    for (var component in _components) {
-      component.zip();
-    }
-  });
 }
