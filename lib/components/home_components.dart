7389c689c408770c7d0cc3c006a0e3b21b9e6ea8
tarturr <tartur.dev@gmail.com>
2024-03-05 19:05:35 +0100

    Added a home-common card component.

---
+++ lib/components/home_components.dart
@@ -0,0 +1,37 @@
+import 'package:admin_panel/components/common/navigation_bar/common.dart';
+import 'package:jaspr/jaspr.dart';
+
+
+typedef Button = ({String label, String href});
+
+class Card extends ClassesContainer {
+  final String title;
+  final String description;
+  final Button button;
+
+  Card({required this.title, required this.description, required this.button, required super.classes}) : super.extraClasses('floating card');
+
+  @override
+  Iterable<Component> build(BuildContext context) {
+    return [
+      div(
+          [
+            h1(
+                [text(title)],
+                classes: 'title'
+            ),
+            p(
+                [text(description)],
+                classes: 'description'
+            ),
+            a(
+                [text(button.label)],
+                href: button.href,
+                classes: 'floating redirect'
+            )
+          ],
+          classes: super.classes
+      )
+    ];
+  }
+}
